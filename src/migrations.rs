fad718552dfdb2a212881def6bb68e63561faaec
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-10-22 09:07:36 +0530

    refactor: update project_dependency schema to include created_at and last_seen_at fields

--- src/migrations.rs
+++ src/migrations.rs
@@ -15,9 +15,10 @@ DEFINE INDEX OVERWRITE idx_last_indexed_at ON dependency FIELDS last_indexed_at;
 static MIGRATION_2025_10_22: &str = r#"
 DEFINE TABLE OVERWRITE project_dependency SCHEMAFULL;
 
-DEFINE FIELD OVERWRITE path ON project_dependency TYPE string;
-DEFINE FIELD OVERWRITE created_at ON project_dependency TYPE datetime VALUE time::now() READONLY;
 DEFINE FIELD OVERWRITE dependency_id ON project_dependency TYPE record<dependency>;
+DEFINE FIELD OVERWRITE path ON project_dependency TYPE string;
+DEFINE FIELD OVERWRITE first_seen_at ON project_dependency TYPE datetime VALUE time::now() READONLY;
+DEFINE FIELD OVERWRITE last_seen_at ON project_dependency TYPE datetime VALUE time::now();
 
 DEFINE INDEX OVERWRITE idx_dependency_id ON project_dependency FIELDS dependency_id;
 "#;
